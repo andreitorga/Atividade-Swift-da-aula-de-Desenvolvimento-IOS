0112a43e457f80d741031990fa73df017d05adf4
andreitorga <73239201+andreitorga@users.noreply.github.com>
2021-03-01 22:15:00 -0300

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,33 +1,28 @@
 
-// 01)
-var nickName:String = "Augustinho";
-print(nickName);
-var name = "Augusto";
+// Da forma mais longa para a mais curta maneira de escrever, declare variáveis em um código Swift.
+var name:String = "Andrei";
+print(name);
+var name = "Andrei";
 
 
-//02)
+//Ao pensar em um sistema de venda de carros, ao cadastrar um novo para venda pense em quais informações seriam constantes e quais seriam variáveis, e declare esses dados em um código Swift.
+var modelo = "";
+var Kilometragem = "";
+let valordocarro = 19000;
+let modeloCarro = "Fusion";
 
-var nameComprador = "";
-var cpfComprador = "";
-let valorCarro = 31000;
-let modeloCarro = "ford fiesta";
+// Descreva seu entendimento sobre anotação de tipos no Swift.
+// As anotações do tipo swith são usadas para declarar o tipo de variavel.
 
-//03)
-// As anotações de tipo no swith são usadas para declara qaul o tipo da variavel.
-// como por exemplo var nickname:String = "Augustinho" neste caso a var nickName está 
-// sendo identificada como String. caso els estiivece sendo declara como var nickName = "Augustinho" futuramente eu poderia atribuir um outro valor como numerioc ou buleano nas variavel de nome nickName
-
-//04)
-var intValue = 1
-var doubleValue = 1.3
+//Declare uma variável do tipo Int e outra do tipo Double e converta as duas para um texto do tipo String em uma terceira variável.
+var intvalue = 1
+var doublevalue = 2,0
 var valueString = String(intValue)
 var valueString = String(doubleValue)
 
-//05)
-
+S
 //type safe: 
-// Swift é uma linguagem de tipo seguro. Uma linguagem de tipo seguro incentiva você a ser claro sobre os tipos de valores com os quais seu código pode trabalhar. Se parte do seu código espera uma String, você não pode passar um Int por engano.
+// Swift é uma linguagem segura. Uma linguagem do tipo segura precisa que você deixe claro os tipos de valores quais seu código pode trabalhar. Se parte do seu código espera uma String, você não pode passar um Int.
 
 //type inference
-
 // Se você não especificar o tipo de valor de que precisa, o Swift usa a inferência de tipo para descobrir o tipo apropriado. A inferência de tipo permite que um compilador deduza o tipo de uma expressão específica automaticamente ao compilar seu código, simplesmente examinando os valores fornecidos.
